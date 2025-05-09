afd8957ee287b490305ad7f303786f628f66c3cf
Nguyen Truong Viet <nguyentruongviet0305@gmail.com>
2025-05-09 17:46:54 +0700

    update

--- src/api/AuthenticateApi.js
+++ src/api/AuthenticateApi.js
@@ -8,6 +8,12 @@ export const sendOtp = async (data) => {
     const url = "/api/v1/user/send-otp";
     return await Instance.post(url, data);
 };
+
+export const verifyOtp = async (data) => {
+    const url = "/api/v1/user/verify-otp";
+    return await Instance.post(url, data);
+};
+
 export const signIn = async (data) => {
     const url = "/api/v1/user/login";
     return await Instance.post(url, data);
@@ -16,6 +22,7 @@ export const changePassword = async (data) => {
     const url = "/api/v1/user/change-password";
     return await Instance.put(url, data);
 };
+
 export const forgotPassword = async (data) => {
     const url = "/api/v1/user/forgot-password";
     return await Instance.post(url, data);
